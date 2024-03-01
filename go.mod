c9d6e2ad79fa49354967a9a6b855568bab1b8c82
Fabian Simon <fabian.simon@inovex.de>
2024-03-01 16:04:30 +0100

    wip

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/fasibio/portainer-api-cli
 go 1.13
 
 require (
-	github.com/urfave/cli v1.22.2
+	github.com/rodaine/table v1.1.1
+	github.com/urfave/cli/v2 v2.27.1
 	go.uber.org/zap v1.13.0
 )
--- main.go
+++ main.go
@@ -3,16 +3,15 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 
 	"github.com/fasibio/portainer-api-cli/logger"
-	"github.com/urfave/cli"
+	"github.com/rodaine/table"
+	"github.com/urfave/cli/v2"
 )
 
 const (
-	CliKeyDeployStack  = "deploystack"
 	CliKeyUserName     = "username"
 	CliKeyUserPassword = "password"
 	CliStackName       = "stack"
@@ -22,80 +21,106 @@ const (
 	CliEndPointID      = "endpoint"
 )
 
-func getEnvName(prefix string) string {
-	return fmt.Sprintf("PORTAINER_API_CLI_%s", strings.ToUpper(prefix))
+func getEnvName(prefix string, path ...string) string {
+	p := strings.Join(path, "_")
+	return fmt.Sprintf("PORTAINER_API_CLI_%s_%s", strings.ToUpper(p), strings.ToUpper(prefix))
 }
 
 func main() {
 	r := Runner{}
-	app := cli.NewApp()
-	app.Name = "portainer-api-cli"
-	app.Action = r.Run
-	app.Before = r.Before
-	app.Commands = []cli.Command{
-		{
-			Name: "create",
-			Subcommands: cli.Commands{
-				cli.Command{
-					Name:   "config",
-					Action: r.CreateConfig,
-					Flags: []cli.Flag{
-						cli.StringFlag{
-							Name:   "name",
-							EnvVar: getEnvName("name"),
-							Usage:  "name of config",
+	app := &cli.App{
+		Name:   "portainer-api-cli",
+		Before: r.Before,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    CliKeyUserName,
+				EnvVars: []string{getEnvName(CliKeyUserName)},
+				Usage:   "username connect to portainer",
+			},
+			&cli.StringFlag{
+				Name:    CliKeyUserPassword,
+				EnvVars: []string{getEnvName(CliKeyUserPassword)},
+				Usage:   "password connect to portainer",
+			},
+			&cli.StringFlag{
+				Name:    CliPortainerUrl,
+				EnvVars: []string{getEnvName(CliPortainerUrl)},
+				Usage:   "Url to you portainer eg. http://portainer:9000",
+			},
+			&cli.StringFlag{
+				Name:    CliEndPointID,
+				EnvVars: []string{getEnvName(CliEndPointID)},
+				Usage:   "Endpoint to use",
+				Value:   "1",
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name: "config",
+				Subcommands: []*cli.Command{
+					{
+						Name:   "rm",
+						Action: r.ConfigRemove,
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name:    "id",
+								EnvVars: []string{getEnvName("id", "config", "rm")},
+							},
 						},
-						cli.StringFlag{
-							Name:   "content",
-							EnvVar: getEnvName("content"),
-							Usage:  "path to dir or - for STDIN",
+					},
+					{
+						Name:   "ls",
+						Action: r.ConfigList,
+					},
+					{
+						Name:   "create",
+						Action: r.ConfigCreate,
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name:    "name",
+								EnvVars: []string{getEnvName("name", "config", "create")},
+								Usage:   "name of config",
+							},
+							&cli.StringFlag{
+								Name:    "content",
+								EnvVars: []string{getEnvName("content", "config", "create")},
+								Usage:   "path to dir or - for STDIN",
+							},
+							&cli.StringSliceFlag{
+								Name:     "labels",
+								EnvVars:  []string{getEnvName("labels", "config", "create")},
+								Required: false,
+							},
 						},
 					},
 				},
 			},
-		},
-	}
-	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:   CliKeyDeployStack,
-			EnvVar: getEnvName(CliKeyDeployStack),
-			Usage:  "Deploy stack to Portainer",
-		},
-		cli.StringFlag{
-			Name:   CliKeyUserName,
-			EnvVar: getEnvName(CliKeyUserName),
-			Usage:  "username connect to portainer",
-		},
-		cli.StringFlag{
-			Name:   CliKeyUserPassword,
-			EnvVar: getEnvName(CliKeyUserPassword),
-			Usage:  "password connect to portainer",
-		},
-		cli.StringFlag{
-			Name:   CliStackName,
-			EnvVar: getEnvName(CliStackName),
-			Usage:  "name of stack you want deploy",
-		},
-		cli.StringFlag{
-			Name:   CliSwarmID,
-			EnvVar: getEnvName(CliSwarmID),
-			Usage:  "the id of your swarm",
-		},
-		cli.StringFlag{
-			Name:   CliComposePath,
-			EnvVar: getEnvName(CliComposePath),
-			Usage:  "set the elasticsearch destination index",
-		},
-		cli.StringFlag{
-			Name:   CliPortainerUrl,
-			EnvVar: getEnvName(CliPortainerUrl),
-			Usage:  "Url to you portainer eg. http://portainer:9000",
-		},
-		cli.StringFlag{
-			Name:   CliEndPointID,
-			EnvVar: getEnvName(CliEndPointID),
-			Usage:  "Endpoint to use",
-			Value:  "1",
+			{
+				Name: "stack",
+				Subcommands: []*cli.Command{
+					{
+						Name:   "deploy",
+						Action: r.StackDeploy,
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name:    CliStackName,
+								EnvVars: []string{getEnvName(CliStackName, "stack", "deploy")},
+								Usage:   "name of stack you want deploy",
+							},
+							&cli.StringFlag{
+								Name:    CliSwarmID,
+								EnvVars: []string{getEnvName(CliSwarmID)},
+								Usage:   "the id of your swarm",
+							},
+							&cli.StringFlag{
+								Name:    CliComposePath,
+								EnvVars: []string{getEnvName(CliComposePath)},
+								Usage:   "path to compose file",
+							},
+						},
+					},
+				},
+			},
 		},
 	}
 	if err := app.Run(os.Args); err != nil {
@@ -116,6 +141,7 @@ func (r *Runner) Before(c *cli.Context) error {
 	logger.Initialize("info")
 	r.api = PortainerApi{
 		PortainerUrl: c.String(CliPortainerUrl),
+		EndpointId:   c.String(CliEndPointID),
 	}
 	err := r.api.Login(c.String(CliKeyUserName), c.String(CliKeyUserPassword))
 	if err != nil {
@@ -124,7 +150,37 @@ func (r *Runner) Before(c *cli.Context) error {
 	return nil
 }
 
-func (r *Runner) CreateConfig(c *cli.Context) error {
+func (r *Runner) ConfigRemove(c *cli.Context) error {
+	return r.api.RemoveConfig(c.String("id"))
+}
+
+func (r *Runner) ConfigList(c *cli.Context) error {
+	configs, err := r.api.ListConfig()
+	if err != nil {
+		return err
+	}
+
+	tbl := table.New("ID", "Name", "UpdatedAt")
+
+	for _, v := range *configs {
+		tbl.AddRow(v.ID, v.Spec.Name, v.UpdatedAt)
+	}
+
+	tbl.Print()
+	return nil
+}
+
+func (r *Runner) ConfigCreate(c *cli.Context) error {
+	lables := c.StringSlice("labels")
+	labelMap := make(map[string]string)
+	for _, v := range lables {
+		split := strings.SplitN(v, "=", 2)
+		if len(split) != 2 {
+			return fmt.Errorf("labels have to be key=value get %s %v", v, split)
+		}
+		labelMap[split[0]] = split[1]
+	}
+
 	contentPath := c.String("content")
 	content := ""
 	if contentPath == "-" {
@@ -143,56 +199,50 @@ func (r *Runner) CreateConfig(c *cli.Context) error {
 		}
 		content = c
 	}
-	log.Println("ioer", content)
-	res, err := r.api.CreateConfig(c.String("name"), content, "1")
+	res, err := r.api.CreateConfig(c.String("name"), content, labelMap)
 	if err == nil {
 		logger.Get().Info(res)
 	}
 	return err
 }
 
-func (r *Runner) Run(c *cli.Context) error {
+func (r *Runner) StackDeploy(c *cli.Context) error {
 	logs := logger.Initialize("info")
 	p := PortainerApi{
 		PortainerUrl: c.String(CliPortainerUrl),
 	}
-	if c.Bool(CliKeyDeployStack) {
-		logs.Info("Deploy Stack")
-		err := p.Login(c.String(CliKeyUserName), c.String(CliKeyUserPassword))
-		if err != nil {
-			return err
-		}
-		composeContent, err := getFileContent(c.String(CliComposePath))
-		if err != nil {
-			return err
-		}
+	logs.Info("Deploy Stack")
+	err := p.Login(c.String(CliKeyUserName), c.String(CliKeyUserPassword))
+	if err != nil {
+		return err
+	}
+	composeContent, err := getFileContent(c.String(CliComposePath))
+	if err != nil {
+		return err
+	}
 
-		feedback, err := p.DeployNewApp(DeployNewStackInformation{
-			Name:             c.String(CliStackName),
-			SwarmID:          c.String(CliSwarmID),
-			StackFileContent: composeContent,
-		}, c.String(CliEndPointID))
-		if err == nil {
-			logs.Info("Deploy New Stack Successful ", feedback)
-			return nil
-		}
-		logs.Info("Error Deploy new Stack perhaps it already exist... try to update", err)
-		id, err := p.GetStackIDByName(c.String(CliStackName))
-		if err != nil {
-			return err
-		}
-		logs.Infow("Find Stack", "ID", id)
-		feedback, err = p.UpdateStack(UpdateStackInfo{
-			Prune:            false,
-			StackFileContent: composeContent,
-		}, id, c.String(CliEndPointID))
-		if err != nil {
-			return err
-		}
-		logs.Info("Update Stack Successful ", feedback)
+	feedback, err := p.DeployNewApp(DeployNewStackInformation{
+		Name:             c.String(CliStackName),
+		SwarmID:          c.String(CliSwarmID),
+		StackFileContent: composeContent,
+	}, c.String(CliEndPointID))
+	if err == nil {
+		logs.Info("Deploy New Stack Successful ", feedback)
 		return nil
 	}
-	logs.Info("No Command choosed")
-
+	logs.Info("Error Deploy new Stack perhaps it already exist... try to update", err)
+	id, err := p.GetStackIDByName(c.String(CliStackName))
+	if err != nil {
+		return err
+	}
+	logs.Infow("Find Stack", "ID", id)
+	feedback, err = p.UpdateStack(UpdateStackInfo{
+		Prune:            false,
+		StackFileContent: composeContent,
+	}, id, c.String(CliEndPointID))
+	if err != nil {
+		return err
+	}
+	logs.Info("Update Stack Successful ", feedback)
 	return nil
 }
